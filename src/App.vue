54d93603103a195f88093bca65b72b6037089bfb
ton4023 <ton4023@gmail.com>
2020-11-27 21:14:51 +0700

    add animation animejs

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <button @click="download">Save</button>
-  <div ref="el" class="container mx-auto flex flex-col justify-items-center">
-    <div class="flex flex-row m-10 items-center">
-      <div class="w-28">
-        <img :src="img" alt="profile" class="rounded-full" />
+  <div ref="el" class="container mx-auto">
+    <div class="flex flex-col justify-items-center">
+      <div class="flex flex-row m-10">
+        <profile />
       </div>
-      <div class="px-10">
-        <h1 class="font-sans text-5xl font-bold">Hello. I'm narongpol</h1>
-        <h2 class="text-2xl pt-4">Web Developer</h2>
+      <div class="flex flex-col mx-10">
+        <about />
+        <contact />
+        <program />
+        <experience />
+        <education />
       </div>
-    </div>
-    <div class="flex flex-col mx-10">
-      <about />
-      <contact />
-      <program />
-      <experience />
-      <education />
     </div>
   </div>
 </template>
@@ -29,6 +25,7 @@ import Contact from "./Components/Contact.vue";
 import Program from "./Components/Program.vue";
 import Education from "./Components/Education.vue";
 import Experience from "./Components/Experience.vue";
+import Profile from "./Components/Profile.vue";
 
 export default defineComponent({
   name: "App",
@@ -38,6 +35,7 @@ export default defineComponent({
     Program,
     Education,
     Experience,
+    Profile,
   },
   setup() {
     const el = ref();
@@ -45,10 +43,7 @@ export default defineComponent({
     const download = () => {
       store.dispatch("download", el);
     };
-    const img = ref(
-      "https://scontent.fbkk22-1.fna.fbcdn.net/v/t1.0-9/17021665_1384850054910808_5868900520486235066_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeFAnlYa2oA2m3xY0Kd5n9OxXcz5Wg6Ej_BdzPlaDoSP8JmGB7mBkWu9XMrox57T9EfhELyFh3Q0LhaM__6ekfdz&_nc_ohc=2z0gTQTiYHUAX_CUzd6&_nc_ht=scontent.fbkk22-1.fna&oh=222c7a74ef77e5cc53cd9a55b14bcc97&oe=5FE6C5FD"
-    );
-    return { el, img, download };
+    return { el, download };
   },
 });
 </script>
---
+++ src/Components/Profile.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-28">
+    <img :src="img" alt="profile" class="rounded-full" />
+  </div>
+  <div ref="profile" class="profile px-10">
+    <h1 class="font-sans text-5xl font-bold">
+      Hello. I'm narongpol
+    </h1>
+    <h2 class="text-2xl pt-4">Web Developer</h2>
+  </div>
+</template>
+<script>
+import { onMounted, ref } from "vue";
+import anime from "animejs";
+export default {
+  name: "Profile",
+  setup() {
+    const profile = ref();
+    const img = ref(
+      "https://scontent.fbkk22-1.fna.fbcdn.net/v/t1.0-9/17021665_1384850054910808_5868900520486235066_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeFAnlYa2oA2m3xY0Kd5n9OxXcz5Wg6Ej_BdzPlaDoSP8JmGB7mBkWu9XMrox57T9EfhELyFh3Q0LhaM__6ekfdz&_nc_ohc=2z0gTQTiYHUAX_CUzd6&_nc_ht=scontent.fbkk22-1.fna&oh=222c7a74ef77e5cc53cd9a55b14bcc97&oe=5FE6C5FD"
+    );
+    onMounted(() => {
+      anime({
+        targets: profile.value,
+        translateX: 50,
+        delay: 1000,
+        easing: "easeInOutSine",
+        duration: 1000,
+      });
+    });
+
+    return { profile, img };
+  },
+};
+</script>
