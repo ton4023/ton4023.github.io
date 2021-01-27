e5bb4bd9f865107c59d050c55c4720abba26eaf9
ton4023 <ton4023@gmail.com>
2021-01-27 13:38:05 +0700

    Add Static

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
-import "./assets/tailwind.css";
-import 'animate.css'
-
-createApp(App)
-  .use(store)
-  .mount("#app");
